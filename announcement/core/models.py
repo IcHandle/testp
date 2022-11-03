34b8c08c59ba8ba46ecddcbd94b7270f3bdd137e
Jees1 <49425409+Jees1@users.noreply.github.com>
2022-11-03 20:29:49 +0200

    Update models.py

--- announcement/core/models.py
+++ announcement/core/models.py
@@ -108,10 +108,13 @@ class AnnouncementModel:
         else:
             color = _color_converter(color)
         embed = discord.Embed(description=description, color=color, timestamp=discord.utils.utcnow())
+        author = self.ctx.author
+        #embed.set_author(name=str(author), icon_url=author.display_avatar)
         if thumbnail_url:
             embed.set_thumbnail(url=thumbnail_url)
         if image_url:
             embed.set_image(url=image_url)
+        embed.set_footer(text="Partners", icon_url=self.channel.guild.icon)
         self.embed = embed
         return embed
 
